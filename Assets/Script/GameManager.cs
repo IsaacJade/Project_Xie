f37887fca025557a72a755f0044ed5c730ea0e5f
Isaac <isaackwan105@gmail.com>
2020-08-12 19:24:06 +0800

    GameManager implemented

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.LogError("Duplicate GameManager");
+        }
+       
+    }
+}
