1e97d30080923463324a8c96e08e168dd37efda7
Isaac <isaackwan105@gmail.com>
2020-08-12 20:56:14 +0800

    Adding Card and two controller

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int id;
+    public string cardName;
+    public int cost;
+    public string cardDescription;
+    public int power;//不是很确定
+    public Card(int Id, string Name, int Cost, string Description, int Power)
+    {
+        this.id = Id;
+        this.cardName = Name;
+        this.cost = Cost;
+        this.cardDescription = Description;
+        this.power = Power;
+    }
+}
