b65e2bc9fb7bd065a0095462f0f8d19e7060958c
Isaac <isaackwan105@gmail.com>
2020-09-12 21:14:11 +0800

    Draw

---
+++ Assets/Script/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+
+}
+static class ListExtension
+{
+    public static T PopAt<T>(this List<T> list, int index)
+    {
+        T r = list[index];
+        list.RemoveAt(index);
+        return r;
+    }
+}
