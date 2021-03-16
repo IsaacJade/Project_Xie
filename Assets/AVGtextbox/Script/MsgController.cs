fd22025a8511bf94bdf2f8aace64a419f4f399f7
LanTian <XIngCHen1176@outlook.com>
2021-03-16 11:59:40 +0800

    add AVGtextbox

---
+++ Assets/AVGtextbox/Script/MsgController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MsgController : MonoBehaviour
+{
+    public Text txt;
+    private static string currentmsg;
+    public static void addmsg(string msg) {
+        currentmsg = currentmsg + "\n"+ "\n" + msg;
+    }
+
+    IEnumerator msgUpdate(){
+        while (true){
+            txt.text = currentmsg;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void OnEnable() {
+        StartCoroutine(msgUpdate());
+    }
+    private void OnDisable(){
+        StopCoroutine(msgUpdate());
+    }
+}
---
+++ Assets/AVGtextbox/Script/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int i = 0;
+    public TextAsset testTxt;
+    IEnumerator Addtext(){
+        //if(Input.GetMouseButtonDown(0)){
+            string[] lines = testTxt.text.Split("\n"[0]);
+            MsgController.addmsg(lines[i]);
+            i++;
+        yield return new WaitForSeconds(2f);
+        //}
+    }
+
+    public void OnClick()
+    {
+        StartCoroutine(Addtext());
+    }
+}
